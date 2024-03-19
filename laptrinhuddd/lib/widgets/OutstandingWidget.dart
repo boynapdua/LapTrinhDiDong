e631ebe16a4cda8c7fd6f5471bbe50331fff9463
HoanKool0205 <hoanlombard0205@gmail.com>
2024-03-19 16:30:21 +0700

    fix Outstanding

--- laptrinhuddd/lib/widgets/OutstandingWidget.dart
+++ laptrinhuddd/lib/widgets/OutstandingWidget.dart
@@ -11,7 +11,7 @@ class OutStandingWidget extends StatelessWidget {
           children: [
 
             //Single Item
-            for(int i=0; i<10; i++)
+            //for(int i=0; i<10; i++)
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 10),
                 child: Container(
@@ -98,6 +98,180 @@ class OutStandingWidget extends StatelessWidget {
                   ),
                 ),
               ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Container(
+                width: 380,
+                height: 150,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10), // Bo góc cho container chứa hình ảnh
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      )
+                    ]
+                ),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15), // Bo góc cho hình ảnh
+                        child: Image.network(
+                          "https://www.thecocktaildb.com/images/media/drink/mrz9091589574515.jpg/preview",
+                          height: 120,
+                          width: 120,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Container(
+                      width: 190,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            "Daiquiri",
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Text(
+                            "Thiết kế giao diện người dùng ứng dụng giao đồ ăn trong Flutter ",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+
+                          RatingBar.builder(
+                            initialRating: 4,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            itemCount: 5,
+                            itemSize: 18,
+                            itemPadding: EdgeInsets.symmetric(horizontal: 4),
+                            itemBuilder: (context, _) =>Icon(
+                              Icons.star,
+                              color: Colors.red,
+                            ),
+                            onRatingUpdate: (index){},
+                          ),
+
+                        ],
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 26,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Container(
+                width: 380,
+                height: 150,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10), // Bo góc cho container chứa hình ảnh
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      )
+                    ]
+                ),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15), // Bo góc cho hình ảnh
+                        child: Image.network(
+                          "https://www.thecocktaildb.com/images/media/drink/k1xatq1504389300.jpg/preview",
+                          height: 120,
+                          width: 120,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Container(
+                      width: 190,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            "Nana Dai",
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Text(
+                            "Thiết kế giao diện người dùng ứng dụng giao đồ ăn trong Flutter ",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+
+                          RatingBar.builder(
+                            initialRating: 4,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            itemCount: 5,
+                            itemSize: 18,
+                            itemPadding: EdgeInsets.symmetric(horizontal: 4),
+                            itemBuilder: (context, _) =>Icon(
+                              Icons.star,
+                              color: Colors.red,
+                            ),
+                            onRatingUpdate: (index){},
+                          ),
+
+                        ],
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 26,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
