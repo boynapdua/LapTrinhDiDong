c2f06ab831e7f82ade702ee731cccdfd7423c259
ptnga <pthuyngak3@gmail.com>
2024-03-21 18:10:45 +0700

    favorite

--- laptrinhuddd/lib/widgets/favourite.dart
+++ laptrinhuddd/lib/widgets/favourite.dart
@@ -11,40 +11,54 @@ class favouriteList extends StatefulWidget {
 class _favouriteListState extends State<favouriteList> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('cocktails').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        if (snapshot.hasError) {
-          return Center(
-            child: Text('Error: ${snapshot.error}'),
-          );
-        }
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Center(
-            child: Text('No cocktails found'),
-          );
-        }
-        return ListView(
-          children: snapshot.data!.docs.map((document) {
-            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-            return ListTile(
-              title: Text(data['strDrink']),
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(data['strDrinkThumb']),
-              ),
-              onTap: () {
-                // Điều hướng đến trang chi tiết của cocktail nếu cần
-              },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favourite Cocktails'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('cocktails').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          }).toList(),
-        );
-      },
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Text('No cocktails found'),
+            );
+          }
+          // setState(() {});
+          return  ListView(
+              children: snapshot.data!.docs.map((document) {
+                Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                return Container(
+                  margin: EdgeInsets.symmetric(vertical: 8),
+                  child: ListTile(
+                    title: Text(data['strDrink'], style: TextStyle(fontSize: 20),),
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(data['strDrinkThumb']),
+                    ),
+                    onTap: () {
+
+                      },
+                  ),
+                );
+
+              }).toList(),
+            );
+
+
+        },
+      )
     );
+
   }
 }
 
